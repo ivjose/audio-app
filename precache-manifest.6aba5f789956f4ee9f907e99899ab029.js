dd02d7d32a0ceaee9a33717e812adabc65298345
ivjose <iv.josesantos@gmail.com>
2020-04-22 23:24:22 +0800

    Updates

---
+++ precache-manifest.6aba5f789956f4ee9f907e99899ab029.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "55658ac135797e093b7cdd182f185b02",
+    "url": "/audio-app/index.html"
+  },
+  {
+    "revision": "b469eca3cc504b2fbb09",
+    "url": "/audio-app/static/js/2.bf6998c9.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/audio-app/static/js/2.bf6998c9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f31501a2fea69dcbbc73",
+    "url": "/audio-app/static/js/main.01d726ad.chunk.js"
+  },
+  {
+    "revision": "d6d00daf2631b53b39de",
+    "url": "/audio-app/static/js/runtime-main.d3566d26.js"
+  },
+  {
+    "revision": "eb171bfbda32d4fd7ae0918b42a7fed2",
+    "url": "/audio-app/static/media/sample-audio.eb171bfb.wav"
+  }
+]);
